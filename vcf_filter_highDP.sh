33aa2262481a3b235c69ac49804f62486216c8d5
zacharyhanna <zachanna@berkeley.edu>
2018-04-10 13:24:50 -0700

    Update vcf_filter_highDP.sh
    Correctly implemented passing the maximumDP command line argument into the awk script.

--- vcf_filter_highDP.sh
+++ vcf_filter_highDP.sh
@@ -2,6 +2,6 @@
 ## Usage:
 ## ./vcf_filter_highDP.sh variants.vcf maximum_depth
 in_vcf=$1
-maxDP=$2
-awk '/^#/ {print $0; next}
-    {for(i=1; i<=(split($8,zfd,";"));i++) if(split(zfd[i],numDP,"DP=")==2 && numDP[2]<$maxDP) {print}}' $in_vcf
+maximumDP=$2
+awk -v maxDP=$maximumDP '/^#/ {print $0; next}
+    {for(i=1; i<=(split($8,zfd,";"));i++) if(split(zfd[i],numDP,"DP=")==2 && numDP[2]<maxDP) {print}}' $in_vcf
