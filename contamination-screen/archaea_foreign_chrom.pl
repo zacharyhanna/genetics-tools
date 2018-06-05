b52bdea4f5dea09598d6c3af94e47fcab470b7c2
zacharyhanna <zachanna@berkeley.edu>
2018-06-05 13:10:34 -0700

    Add taxonomic database fetching scripts via upload

---
+++ contamination-screen/archaea_foreign_chrom.pl
@@ -0,0 +1,29 @@
+use LWP::Simple;
+$query = 'archaea%5Bporgn%5D+AND+srcdb_refseq%5Bprop%5D+AND+biomol_genomic%5Bprop%5D+AND+complete%5Bprop%5D';
+
+#assemble the esearch URL
+$base = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/';
+$url = $base . "esearch.fcgi?db=nuccore&term=${query}&usehistory=y";
+
+#post the esearch URL
+$output = get($url);
+
+#parse WebEnv, QueryKey and Count (# records retrieved)
+$web = $1 if ($output =~ /<WebEnv>(\S+)<\/WebEnv>/);
+$key = $1 if ($output =~ /<QueryKey>(\d+)<\/QueryKey>/);
+$count = $1 if ($output =~ /<Count>(\d+)<\/Count>/);
+
+#open output file for writing
+my $filename = 'archaea_chrom.fa';
+open(my $fh, '>', $filename) or die "Can't open file!\n";
+
+#retrieve data in batches of 500
+$retmax = 500;
+for ($retstart = 0; $retstart < $count; $retstart += $retmax) {
+        $efetch_url = $base ."efetch.fcgi?db=nucleotide&WebEnv=$web";
+        $efetch_url .= "&query_key=$key&retstart=$retstart";
+        $efetch_url .= "&retmax=$retmax&rettype=fasta&retmode=text";
+        $efetch_out = get($efetch_url);
+        print $fh "$efetch_out";
+}
+close $fh;
---
+++ contamination-screen/arthropoda_foreign_chrom.pl
@@ -0,0 +1,28 @@
+use LWP::Simple;
+$query = 'arthropoda%5Bporgn%5D+AND+srcdb_refseq%5Bprop%5D+AND+biomol_genomic%5Bprop%5D+AND+(NC_000000%3ANC_999999%5Bpacc%5D+OR+AC_000000%3AAC_999999%5Bpacc%5D+OR+(NT_000001%3ANT_999999999%5Bpacc%5D+AND+(%22chromosome+2L%22+OR+%22chromosome+2R%22+OR+%22chromosome+3L%22+OR+%22chromosome+3R%22)))';
+#assemble the esearch URL
+$base = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/';
+$url = $base . "esearch.fcgi?db=nuccore&term=${query}&usehistory=y";
+
+#post the esearch URL
+$output = get($url);
+
+#parse WebEnv, QueryKey and Count (# records retrieved)
+$web = $1 if ($output =~ /<WebEnv>(\S+)<\/WebEnv>/);
+$key = $1 if ($output =~ /<QueryKey>(\d+)<\/QueryKey>/);
+$count = $1 if ($output =~ /<Count>(\d+)<\/Count>/);
+
+#open output file for writing
+my $filename = 'arthropoda_chrom.fa';
+open(my $fh, '>', $filename) or die "Can't open file!\n";
+
+#retrieve data in batches of 500
+$retmax = 500;
+for ($retstart = 0; $retstart < $count; $retstart += $retmax) {
+        $efetch_url = $base ."efetch.fcgi?db=nucleotide&WebEnv=$web";
+        $efetch_url .= "&query_key=$key&retstart=$retstart";
+        $efetch_url .= "&retmax=$retmax&rettype=fasta&retmode=text";
+        $efetch_out = get($efetch_url);
+        print $fh "$efetch_out";
+}
+close $fh;
